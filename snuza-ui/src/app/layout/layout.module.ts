df2744d0e3d8928317608677c13f524cf12f73f3
Ernst <Ernst@DESKTOP-9221F8F>
2017-07-13 11:09:31 +0200

    Test Angular

---
+++ snuza-ui/src/app/layout/layout.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { HeaderComponent, SidebarComponent } from '../shared';
+import { DevicesInfoComponent } from './devices-info/devices-info.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        NgbDropdownModule.forRoot(),
+        LayoutRoutingModule,
+        TranslateModule
+    ],
+    declarations: [
+        LayoutComponent,
+        HeaderComponent,
+        SidebarComponent,
+        DevicesInfoComponent,
+    ]
+})
+export class LayoutModule { }
