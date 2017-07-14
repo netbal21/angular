c780097b7cbadeb91726412f5571c45e5c92485f
Ernst <netbal21@gmail.com>
2017-07-14 09:33:20 +0200

    Remove some routing

--- snuza-ui/src/app/layout/layout.module.ts
+++ snuza-ui/src/app/layout/layout.module.ts
@@ -6,7 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent, SidebarComponent } from '../shared';
-import { SubscriptionComponent } from './subscription/subscription.component';
+
 
 @NgModule({
     imports: [
@@ -19,7 +19,8 @@ import { SubscriptionComponent } from './subscription/subscription.component';
         LayoutComponent,
         HeaderComponent,
         SidebarComponent,
-        SubscriptionComponent,
+ 
     ]
 })
+
 export class LayoutModule { }
